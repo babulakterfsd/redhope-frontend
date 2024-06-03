133c3204dfd88850fe86a33f7ec45507e392eb3a
Babul Akter <babulakterfsd@gmail.com>
2024-06-03 19:19:18 +0600

    homepage completed

--- src/util/authOptions.ts
+++ src/util/authOptions.ts
@@ -20,10 +20,13 @@ export const authOptions = {
       async authorize(credentials, req) {
         try {
           const { email, password } = credentials as TCredentials;
-          const res = await axios.post(`${process.env.SERVER_URL}/auth/login`, {
-            email,
-            password,
-          });
+          const res = await axios.post(
+            `https://redhope-backend.vercel.app/api/auth/login`,
+            {
+              email,
+              password,
+            }
+          );
           if (res?.data?.data?.user) {
             console.log('res.data.user', res.data.user);
             return { ...res?.data?.data?.user, token: res?.data?.data?.token };
