fb9dbbe4b16ddcfc26e7924ab75216e185c0539f
Babul Akter <babulakterfsd@gmail.com>
2024-06-05 13:02:22 +0600

    requests to me done

--- src/components/dashboard/DonorDashboard.tsx
+++ src/components/dashboard/DonorDashboard.tsx
@@ -1,13 +1,43 @@
+'use client';
+
 import NotFound from '@/app/not-found';
+import { signOut } from 'next-auth/react';
+import { useState } from 'react';
+import { IoIosLogOut } from 'react-icons/io';
+import { toast } from 'sonner';
 
 const DonorDashboard = ({ loggedInUser }: any) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   if (loggedInUser?.role !== 'donor') {
     return <NotFound />;
   }
 
+  const handleLogout = async () => {
+    await signOut();
+    toast.success('Logout Successful', {
+      position: 'top-right',
+      icon: '👏',
+      duration: 1500,
+    });
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
       <h3>This is donor dashboard</h3>
+      <button
+        className="text-white bg-red-400 px-6 py-2 hover:text-red-300 rounded mt-5"
+        onClick={handleLogout}
+      >
+        <div
+          className="flex justify-start -ml-5 items-center space-x-2"
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+        >
+          <IoIosLogOut className="text-white text-xl font-bold" />
+          <span>Logout</span>
+        </div>
+      </button>
     </div>
   );
 };
--- src/components/dashboard/RequestsToMe.tsx
+++ src/components/dashboard/RequestsToMe.tsx
@@ -1,8 +1,201 @@
+'use client';
+
+import { TBloodRequest } from '@/types/common.types';
+import { useEffect, useState } from 'react';
+
 const RequestsToMe = ({ loggedInUser }: any) => {
-  console.log('loggedInUser in requests to me page', loggedInUser);
+  const [isLoading, setIsLoading] = useState(false);
+  const [requestsToMe, setRequestsToMe] = useState({} as any);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [page, setPage] = useState<string>('1');
+  const limit = '10';
+
+  const handlePageChange = (page: number) => {
+    setPage(page.toString());
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/bloodrequests/requests-made-to-me?page=${page}&limit=${limit}&donorEmail=${loggedInUser?.email}`
+        );
+        const data = await response.json();
+        const requestsToMe = data?.data;
+        setRequestsToMe(requestsToMe);
+        setTotalItems(requestsToMe?.meta?.total);
+        setIsLoading(false);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, [loggedInUser?.email]);
+
+  const totalPages = Math.ceil(Number(totalItems) / Number(limit));
+
+  console.log(requestsToMe);
+
   return (
     <div>
-      <p>This is requests to me page</p>
+      <h3 className="text-center mt-10 lg:mt-14 text-2xl">
+        Blood Requests To Me
+      </h3>
+      <p className="text-center lg:mt-2 md:text-md lg:w-2/3 lg:mx-auto">
+        In this section, you can view all the blood requests made to you. You
+        can view the status of the request and also can accept or reject the
+        request. Once you accept the request, the requesters contact info will
+        be shared with you.
+      </p>
+      {isLoading ? (
+        <div className="mt-10 flex justify-center items-center">
+          <div className="flex justify-center items-center">
+            <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-red-500"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="py-4 px-2">
+          {/* product list table */}
+          <div className="mt-8 min-h-screen rounded-xl lg:py-6 lg:px-4">
+            <div className="relative overflow-x-auto">
+              <table className="w-full lg:w-10/12 lg:mx-auto rounded py-16 px-10 text-sm text-left rtl:text-right text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Requester Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Requester Email
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Requester Mobile
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Blood Group Needed
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Request Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {!isLoading && requestsToMe?.bloodRequests?.length === 0 ? (
+                    <tr>
+                      <td className="text-red-400 font-semibold whitespace-nowrap py-8 pl-12">
+                        No Requests Found
+                      </td>
+                    </tr>
+                  ) : null}
+                  {!isLoading && requestsToMe?.bloodRequests?.length > 0
+                    ? requestsToMe?.bloodRequests?.map(
+                        (request: TBloodRequest) => (
+                          <tr
+                            className="bg-white border-b  hover:bg-red-50"
+                            key={request._id}
+                          >
+                            <th
+                              scope="row"
+                              className="px-6 py-4 font-medium text-gray-700 whitespace-nowrap"
+                            >
+                              {request?.requester?.name}
+                            </th>
+                            <td
+                              className={`px-6 py-4 ${
+                                request?.requestStatus === 'pending'
+                                  ? 'text-red-400'
+                                  : ''
+                              }`}
+                            >
+                              {request?.requestStatus === 'accepted' &&
+                                request?.requester?.email}
+                              {(request?.requestStatus === 'pending' ||
+                                request?.requestStatus === 'rejected') &&
+                                'Hidden'}
+                            </td>
+                            <td
+                              className={`px-6 py-4 ${
+                                request?.requestStatus === 'pending'
+                                  ? 'text-red-400'
+                                  : ''
+                              }`}
+                            >
+                              {request?.requestStatus === 'accepted' &&
+                                request?.requester?.location?.mobile}
+                              {(request?.requestStatus === 'pending' ||
+                                request?.requestStatus === 'rejected') &&
+                                'Hidden'}
+                            </td>
+                            <td className="px-6 py-4">
+                              {request?.donor?.bloodGroup}
+                            </td>
+                            <td
+                              className={`px-6 py-4 ${
+                                request?.requestStatus === 'pending'
+                                  ? 'text-red-400 font-bold'
+                                  : request?.requestStatus === 'rejected'
+                                  ? 'text-red-400 font-bold'
+                                  : 'text-green-400 font-bold'
+                              }`}
+                            >
+                              {request?.requestStatus}
+                            </td>
+                          </tr>
+                        )
+                      )
+                    : null}
+                </tbody>
+              </table>
+
+              {/* pagination */}
+              {isLoading || requestsToMe?.bloodRequests?.length === 0 ? (
+                <div></div>
+              ) : (
+                <div
+                  className={`flex mr-2 lg:mr-28 justify-end items-center my-5 ${
+                    requestsToMe?.bloodRequests?.length < 5
+                      ? 'mt-[200px]'
+                      : 'mt-4'
+                  }`}
+                >
+                  <button
+                    className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-200"
+                    onClick={() => handlePageChange(Number(page) - 1)}
+                    disabled={Number(page) === 1}
+                  >
+                    Prev
+                  </button>
+                  {[...Array(Math.min(totalPages, 5)).keys()].map((index) => {
+                    const pageNumber = Number(page) - 2 + index;
+                    // Check if pageNumber is within valid range and greater than 0
+                    if (pageNumber > 0 && pageNumber <= totalPages) {
+                      return (
+                        <button
+                          key={pageNumber}
+                          className={`mx-2 px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-200 ${
+                            Number(page) === pageNumber ? 'font-bold' : ''
+                          }`}
+                          onClick={() => handlePageChange(pageNumber)}
+                          disabled={Number(page) === pageNumber}
+                        >
+                          {pageNumber}
+                        </button>
+                      );
+                    }
+                    return null; // Render nothing for invalid pageNumber
+                  })}
+                  <button
+                    className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 disabled:cursor-not-allowed disabled:hover:bg-gray-200 ml-2"
+                    onClick={() => handlePageChange(Number(page) + 1)}
+                    disabled={Number(page) === totalPages}
+                  >
+                    Next
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
